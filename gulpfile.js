1f95b3cfe4c0a44cebf7609267540e4f6ebeaeb5
David <hudahai82@hotmail.com>
2018-12-20 16:17:13 +1030

    completed part of blog

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+require('./gulp/task/styles');
+require('./gulp/task/watch');
+
+// var gulp=require('gulp'),
+// watch=require('gulp-watch'),
+
+
+
+// gulp.task('default',function(){
+//   console.log("you create a gulp task");
+// });
+
+// gulp.task('html',function(){
+//   console.log("imagine something useful for my html");
+// });
+
+
+
+
+
